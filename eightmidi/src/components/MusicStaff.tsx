cda7869dd976e3d0036baeaa550e2cb9cf47ea70
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-05-21 18:09:20 -0700

    Add variables for rows and cols

--- eightmidi/src/components/MusicStaff.tsx
+++ eightmidi/src/components/MusicStaff.tsx
@@ -1,8 +1,11 @@
 import NoteContainer from "./NoteContainer"
 
+const COLS = 32;
+const ROWS = 9
+
 function MusicStaff() {
     let notecontainers = []
-    for(let i = 0; i< 32*8; i++){
+    for(let i = 0; i< COLS*ROWS; i++){
         notecontainers.push( <NoteContainer key={i}>{i}</NoteContainer>)
     }
 
