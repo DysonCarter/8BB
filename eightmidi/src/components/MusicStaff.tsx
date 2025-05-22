daa68f04d3155cf0dda3a1b5a89f75a88526f1a1
DysonCarter <129555170+DysonCarter@users.noreply.github.com>
2025-05-21 19:22:33 -0700

    Fix state + Allow one cell per column to be active

--- eightmidi/src/components/MusicStaff.tsx
+++ eightmidi/src/components/MusicStaff.tsx
@@ -1,22 +1,41 @@
 import NoteContainer from "./NoteContainer"
+import { useState } from "react";
 
 const COLS = 32;
 const ROWS = 9
 
 function MusicStaff() {
+    const [activeIndices, setActiveIdices] = useState<number[]>(Array(COLS).fill(ROWS-1))
 
-    let notecontainers = []
-    for(let i = 0; i< COLS*ROWS; i++){
-        if ((i+1) % (ROWS) !== 0){
-            notecontainers.push( <NoteContainer key={i}>{i}</NoteContainer>)
-        } else {
-            notecontainers.push( <NoteContainer key={i} active={true}>{i}</NoteContainer>)
+    const handleNoteClick = (row: number, column: number) => {
+        setActiveIdices((prev) => {
+            const newActive = [...prev];
+            newActive[column] = row;
+            return newActive;
+        });
+    }
+
+    let noteContainers = [];
+    for(let column = 0; column < COLS; column++){
+        for(let row = 0; row < ROWS; row++){
+            const index = column * ROWS + row;
+            const isActive = activeIndices[column] === row;
+            
+            noteContainers.push(
+                <NoteContainer
+                  key={index}
+                  active={isActive}
+                  onClick={()=>{handleNoteClick(row, column)}}
+                >
+                   {index} 
+                </NoteContainer>
+            )
         }
     }
 
     return <>
     <div className="musicGrid">
-        {notecontainers}
+        {noteContainers}
     </div>
     </>
 }
--- eightmidi/src/components/NoteContainer.tsx
+++ eightmidi/src/components/NoteContainer.tsx
@@ -1,25 +1,19 @@
-import { useRef, useState } from "react";
+
 
 type NoteContainerProps = {
     children?: React.ReactNode;
     active?: boolean;
+    onClick?: ()=> void;
 }
 
-function NoteContainer({children, active=false} : NoteContainerProps) {
-
-    const [noteActive, setNoteActive] = useState<boolean>(active)
-    const divRef = useRef<HTMLDivElement>(null);
-
-    function handleNoteClick() {
-        //toggle color:
-        if(divRef.current){
-            divRef.current.style.backgroundColor = noteActive ? "rgb(198, 188, 137)" : "green"
-        }
-        setNoteActive(!noteActive);
-    }
-
+function NoteContainer({children, active=false, onClick} : NoteContainerProps) {
     return <>
-      <div className="noteContainer" ref={divRef} style={{backgroundColor: noteActive ? "green" : "rgb(198, 188, 137)"}}onClick={handleNoteClick}>{children}</div>
+      <div 
+      className="noteContainer"
+      style={{backgroundColor: active ? "green" : "rgb(198, 188, 137)"}}
+      onClick={onClick}>
+        {children}
+      </div>
     </>
 }
 
